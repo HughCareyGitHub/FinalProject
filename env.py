4a7bbe361991314048f700e91b18c98f2b9c2e49
HughCareyGitHub <hgh_carey@yahoo.ie>
2024-07-31 09:58:01 +0000

    Deployment Commit

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ['DATABASE_URL'] = 'postgres://ubxssxoescm:VLvX6Bpwb7j5@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/folk_case_curve_296550'
+os.environ['SECRET_KEY'] = 'secretkey12345'
+
+os.environ['CLOUDINARY_URL']='cloudinary://515944568877862:KNE7o-3FBKlBmbgotNiVZ6ohB1k@ds9atsvur'
