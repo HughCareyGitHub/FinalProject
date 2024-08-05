c9c913f66ce4e57f2f9345df607ed9afd128ee74
HughCareyGitHub <hgh_carey@yahoo.ie>
2024-08-05 22:21:02 +0000

    activate the application

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blogapp.apps.BlogConfig'
     'cloudinary_storage',
     'cloudinary',
     'blogapp',
